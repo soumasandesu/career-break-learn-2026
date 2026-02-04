00b92b800f7d94bc5138b13f848ea6538a839df0
Charles Poon <hkgsherlock@gmail.com>
2026-02-04 10:26:14 +0800

    ADD scratch to impl spring reactive + kafka app proc usr ev stream?

---
+++ user-activity-event-processor/src/main/java/com/test/charles/user_activity_event_processor/UserActivityEventProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.test.charles.user_activity_event_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserActivityEventProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserActivityEventProcessorApplication.class, args);
+	}
+
+}
---
+++ user-activity-event-processor/src/test/java/com/test/charles/user_activity_event_processor/UserActivityEventProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.charles.user_activity_event_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActivityEventProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
